189f784d8683d7d66bcff2f7640d9bbdef431853
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-09-12 16:09:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function spy() {
-  console.log(HELLO.toLowerCase)
+function spy(string) {
+  console.log(string.toLowerCase)
 }
